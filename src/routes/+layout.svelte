3eacb0be40c75bde5e19a20194bc1e7055df3a7c
alexhercko <alexhercko@proton.me>
2026-06-28 20:20:33 +0200

    feat: basic nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { resolve } from '$app/paths';
+    import { page } from '$app/state';
     import favicon from '$lib/assets/favicon.svg';
     import '../app.css';
 
@@ -9,4 +11,35 @@
     <link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav>
+    <a href={resolve('/')} aria-current={page.url.pathname === '/' ? 'page' : undefined}> Home </a>
+    <a href={resolve('/about')} aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
+        About
+    </a>
+</nav>
+
 {@render children()}
+
+<style>
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding: 0.4rem;
+        border-bottom: 1px solid var(--color-text-secondary);
+    }
+
+    nav a {
+        color: var(--color-text-secondary);
+        text-decoration: none;
+        margin: 1rem;
+        font-weight: 300;
+        text-transform: uppercase;
+        font-size: 0.9rem;
+    }
+
+    nav a[aria-current='page'] {
+        color: var(--color-text);
+        font-weight: 400;
+    }
+</style>
